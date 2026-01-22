cd5a0c4f9ecc1b38b4e046ce3d8fbfd6eaa1aef3
GimaShinta <shinta-gima-k23f@stu.kbc.ac.jp>
2026-01-22 09:39:01 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "calc.h"
+
+int main() {
+    std::cout << add(4, 2) << std::endl;
+    std::cout << sub(4, 2) << std::endl;
+    std::cout << mul(4, 2) << std::endl;
+    std::cout << divi(4, 2) << std::endl;
+    return 0;
+}
